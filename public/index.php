7239930c19ea5d854e5e1bcd916aa095fa4919ca
SergeyZhigalo <seregax@icloud.com>
2024-04-30 00:38:17 +0300

    Добавил парсер ics

--- public/index.php
+++ public/index.php
@@ -4,4 +4,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__.'/boot.php';
 
+use IcsReader\IcsReader;
+
+$file = file_get_contents(__DIR__.'/schedule/export_202404300026.ics');
+
+$reader = new IcsReader();
+$ics = $reader->parse($file);
+
+dd(collect($ics->getCalendar()["VEVENT"])->first());
+
 pdo();
