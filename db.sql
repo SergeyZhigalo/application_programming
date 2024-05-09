5b48893564055f733e274d952c74785c365390f3
SergeyZhigalo <seregax@icloud.com>
2024-05-10 00:19:41 +0300

    Добавил sql файл для бд

---
+++ db.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS class_groups(
+    id SERIAL PRIMARY KEY,
+    group_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS universities(
+    id SERIAL PRIMARY KEY,
+    university_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS teachers(
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password text null
+);
+
+CREATE TABLE IF NOT EXISTS students(
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    group_id int NOT NULL,
+    group_head bool NOT NULL,
+    password text null,
+    FOREIGN KEY (group_id) REFERENCES class_groups(id)
+);
+
+CREATE TABLE IF NOT EXISTS classes(
+    id SERIAL PRIMARY KEY,
+    class_start timestamp NOT NULL,
+    class_end timestamp NOT NULL,
+    place TEXT NOT NULL,
+    university_id int NOT NULL,
+    group_id int NOT NULL,
+    teacher_id int NOT NULL,
+    subject TEXT NOT NULL,
+    uid uuid NOT NULL,
+    FOREIGN KEY (university_id) REFERENCES universities(id),
+    FOREIGN KEY (group_id) REFERENCES class_groups(id),
+    FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+);
+
+CREATE TABLE IF NOT EXISTS class_attendance(
+    id SERIAL PRIMARY KEY,
+    class_id int NOT NULL,
+    student_id int NOT NULL,
+    status NUMERIC(2,1) NULL,
+    FOREIGN KEY (class_id) REFERENCES classes(id),
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
